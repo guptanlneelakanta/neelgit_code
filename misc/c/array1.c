5eef6eec3ebd4c860034966319e9ac96484a7ef9
guptanlneelakanta <guptanlneelakanta@gmail.com>
2026-04-28 08:37:13 +0530

    test code

---
+++ misc/c/array1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int arr[5]={10,20,30,40,50};
+    for(int i=0; i<5; i++)
+    {
+        printf("\n value of arr[%d] = %d", i, arr[i]);
+    }    
+    
+    return 0;
+}
+
